74a667b84a4f3677b55ee7c1a939a07cf06ef490
Nurjahan Swarna <nurjahanswarna999@gmail.com>
2024-10-05 03:50:28 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+// Math No.1
+
+let sara_has_apples = 25;
+let she_bought_apples = 15;
+let total_apple = sara_has_apples + she_bought_apples;
+ console.log('saras total apple are' + ' ' + total_apple);
+
+//  The output of question 1 is = 40.
+
+// Math No.2
+
+let JpnHasMoney = 500;
+let SpentAtTheGroceryStore = 200;
+let Isleft = JpnHasMoney - SpentAtTheGroceryStore;
+console.log('Now he has' +' ' + Isleft + ' Tk');
+
+// The output of question 2 is = 300..
+
+
+// Math Node.3
+let Make_the_machine_in_one_hour = 120;
+let Hours = 8;
+let Total_machinery = Make_the_machine_in_one_hour * Hours;
+console.log('Will_make_the_machine in 8 hours'  + ' ' + Total_machinery);
+
+//The output of question 3 is = 120.
+
+// Math No.4
+
+let There_are_trees_in_1_row = 12;
+ let Total_row_has = 8;
+ let Total_trees = There_are_trees_in_1_row * Total_row_has;
+ let add_more_tree = 20;
+ let than_total_tree = Total_trees + add_more_tree;
+ console.log('The tree will be here' + ' ' + than_total_tree );
+
+//  The output of question 4 is = 116.
+
+let Rahim_has_box = 5;
+let one_box_contains_orange = 10;
+let The_total_orange = Rahim_has_box * one_box_contains_orange;
+let lost_orange = 12;
+let He_gave_orange_his_friend = The_total_orange - lost_orange;
+ console.log('Now Rahim has' + ' ' + He_gave_orange_his_friend + ' ' + 'orange');
+
+//  The output of question 5 is = 38.
+
+// Math Node.6
+
+let The_train_overtake = 150;
+let Total_Per_hours = 3;
+let one_per_hour = The_train_overtake / Total_Per_hours;
+console.log('per_hour_train_run' + ' ' + one_per_hour + ' ' + 'k.m ')
+
+// The output of question 6 is = 50.
+
+// Math Node.7
+let present_machine = 6;
+let Again_add_machine = 2;
+let per_day_produce_the_product = 200;
+let not_sale = 3;
+let Total_machine = present_machine + Again_add_machine;
+let total_produce_product = Total_machine * per_day_produce_the_product;
+let if_three_product_are_not_sale = total_produce_product - not_sale;
+console.log('Remains_the_product' + ' ' + if_three_product_are_not_sale )
+
+// The output of question 7 is = 1597.
+
+// math No.8
+let total_chocolet =120;
+let One_packet_of_chocolates = 9;
+let The_shopkeeper_has_chocolates_left = total_chocolet % One_packet_of_chocolates;
+console.log('The_shopkeeper_has_chocolates_left' + ' ' + The_shopkeeper_has_chocolates_left);
+
+// The output of question 8 is = 3.
